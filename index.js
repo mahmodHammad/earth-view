0775375631ef868c86afd2432d8cfc0ce2a6b869
mahmoudhammad <hodaman2012@gmail.com>
2019-09-11 22:25:49 +0200

    get center , need some fix at line 32

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const availableDates='https://epic.gsfc.nasa.gov/api/natural/available?api_key=4sXteRBfA51qWHoLzWocOdu3DcXcW67b9LZjW0C6'
 
-let image=document.querySelector('.mimg')
+let image     =document.querySelector('.mimg')
 let container =document.querySelector('.x')
+let submit    =document.querySelector('#btn')
+let minlat , maxlat, minlong, maxlong
 
-fetch(availableDates).then((f)=>f.json()).then((arrOfAllDates)=>dis12(arrOfAllDates))
+submit.addEventListener('click',()=>{
+     minlat    =document.querySelector('#minlat').value
+     maxlat    =document.querySelector('#maxlat').value
+     minlong   =document.querySelector('#minlong').value
+     maxlong   =document.querySelector('#maxlong').value
+
+     //more input validations must be writen later  
+     if(minlat!='' &&maxlong!='' && minlong!=''&&maxlat!='')
+        fetch(availableDates).then((f)=>f.json()).then((arrOfAllDates)=>dis12(arrOfAllDates))
+})
 
 function dis12(arr){
-    for(let i=0  ;i< arr.length ; i++)
+    for(let i=0  ;i< 900 ; i++)
         getJson(arr[i])       
 }
    
@@ -17,10 +28,15 @@ function getJson (idate){
     
 function select(array){
     for(let i of array){
-        if(i.centroid_coordinates.lat<25&&i.centroid_coordinates.lat>14 
-        &&i.centroid_coordinates.lon<22 &&i.centroid_coordinates.lon>15 ){
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX IM MUST MAKE THIS DAMN CONDITION VALID FOR NEGATIVE VALUES xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+       
+        if(( i.centroid_coordinates.lat) <(maxlat)  && ( i.centroid_coordinates.lat)>( minlat)  
+        && ( i.centroid_coordinates.lon) <(maxlong) && ( i.centroid_coordinates.lon)>( minlong) ){    
             playwithJson(i)   
         }
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//   console.log(array)
     }
 }
 
